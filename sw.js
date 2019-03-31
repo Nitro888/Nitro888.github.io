a6ff5bb464237c2ad0292befef17330cd8988df0
Nitro888 <nitro88x@gmail.com>
2019-03-31 14:59:17 +0900

    nuxt bugfix

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/4f3d33bdae1a8fb68a34.js",
-    "revision": "8dfe31616e87297d7753dac33202c74a"
+    "url": "/_nuxt/1402946040d28ed483bb.js",
+    "revision": "7c649171c53dcdefa97595b1ca7b2344"
   },
   {
-    "url": "/_nuxt/541c10134d52f47cbde3.js",
-    "revision": "91ea9f53f863a27ec1412b83ee93fce5"
+    "url": "/_nuxt/24bf0c79eafdaebbe491.js",
+    "revision": "ddf17de27c02769037f66b1429ad33c2"
+  },
+  {
+    "url": "/_nuxt/84c2473539fe22ac4ed4.js",
+    "revision": "dfb410d1c9140fc156f589cdfaff7e09"
   },
   {
     "url": "/_nuxt/c74bf94161b9a5a00b51.js",
     "revision": "8dbe32edcfdcf34e5b1a1a9a1fe45cc9"
-  },
-  {
-    "url": "/_nuxt/f38e35863008e3bbc3ae.js",
-    "revision": "f23fc20eea55255533036fe6604362ad"
   }
 ], {
   "cacheId": "daoauth",
