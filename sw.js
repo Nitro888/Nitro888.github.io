901b65131304df3ae46a5fd14408e87a5326fedf
Nitro888 <nitro88x@gmail.com>
2019-03-31 17:40:56 +0900

    fix warning

--- sw.js
+++ sw.js
@@ -2,16 +2,16 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/07da37aa7dea9071ab08.js",
+    "revision": "1d01e211cb4c58af47ceeb32d6de1889"
+  },
+  {
     "url": "/_nuxt/14a0cc0b031220978e17.js",
     "revision": "5108c9cae2e1695223af629a13755b83"
   },
   {
     "url": "/_nuxt/772d933113dba04644b9.js",
     "revision": "219bac420274dec4ee2acd935efc20fc"
-  },
-  {
-    "url": "/_nuxt/bbea79ebeee572d9ddf4.js",
-    "revision": "dc75632212490012e2937b8cdc65e191"
   },
   {
     "url": "/_nuxt/c74bf94161b9a5a00b51.js",
